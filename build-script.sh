3f904217192cb80e7e91d799559771fd3ee0b4c8
taichanne30 <dev@taichan.site>
2023-06-21 22:13:41 +0900

    Add mecab on build

--- build-script.sh
+++ build-script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 npm i -g pnpm && pnpm i && pnpm build
+
+sudo apt -y update && sudo apt -y install mecab libmecab-dev mecab-utils mecab-ipadic-utf8 git make curl xz-utils file
+
+git clone --depth 1 https://github.com/neologd/mecab-ipadic-neologd.git
+
+cd mecab-ipadic-neologd
+./bin/install-mecab-ipadic-neologd -n -a -y
